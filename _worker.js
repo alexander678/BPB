d32831f05dbba7b1e69d4e9b56d1b224fde2f48f
alexander678 <64352576+alexander678@users.noreply.github.com>
2024-10-04 13:34:09 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '31623235-4d27-4102-a40c-dc7b26606d80';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
